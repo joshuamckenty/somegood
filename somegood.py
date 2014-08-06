a42269e61a41f39e2bd9e6fb5f451b80632d203f
Joshua McKenty <joshua@pistoncloud.com>
2014-08-05 17:24:34 -0700

    Adding the orchestrate.io client.

--- somegood.py
+++ somegood.py
@@ -1,5 +1,18 @@
+"""
+SomeGood's main flask app.
+
+This is a placeholder for now.
+
+"""
+
+import os
+from porc import Client
 from flask import Flask
+
 app = Flask(__name__)
+
+client = Client(os.environ['ORCHESTRATE_KEY'])
+client.ping().raise_for_status()
 
 @app.route('/')
 def hello_world():
